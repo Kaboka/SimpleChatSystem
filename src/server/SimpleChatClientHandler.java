04fd7c27d06a0851e7cc4239607894c08d8c02f3
Nicklas Hemmingsen <Nicklas Hemmingsen@10.99.19.189>
2013-03-05 13:34:21 +0100

    First Servlet Example commit

---
+++ src/server/SimpleChatClientHandler.java
@@ -0,0 +1,27 @@
+
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SimpleChatClientHandler extends Thread {
+    
+    public static void handleClient (Socket socket) throws IOException{
+                //Don't forget to turn on autoFlush
+        PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
+        Scanner input = new Scanner(socket.getInputStream());
+        //Important: This is a blocking call.
+        String msg = input.nextLine();
+        while(input.hasNext()){
+            output.println(msg);
+            msg = input.nextLine();
+        }
+        socket.close();
+    }
+    
+    public void run(){
+        
+    }
+}
--- src/server/SimpleChatServer.java
+++ src/server/SimpleChatServer.java
@@ -4,13 +4,40 @@
  */
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Nicklas Hemmingsen
  */
 public class SimpleChatServer {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static final int PORT = 1234;
+    static ServerSocket serverSocket;
+    public static SimpleChatClientHandler ch;
+    private static Map<String, Socket> clients;
+    
+        public static void main(String[] args) {
+        System.out.println("Server is started");
+        clients = new HashMap<>();
+        try {
+            serverSocket = new ServerSocket(PORT); 
+            while(true){     
+                ch = new SimpleChatClientHandler();
+                //Important: This is a blocking call.
+                Socket socket = serverSocket.accept();
+                SimpleChatClientHandler.handleClient(socket);
+                System.out.println("Got a new client");
+                clients.put("", socket);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(SimpleChatServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
